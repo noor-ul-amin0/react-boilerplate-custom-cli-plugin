5ddad0951d2a677ddfe459f7b9937cf7df77b380
Noor Ul Amin <noorawan444@gmail.com>
2023-10-09 14:57:09 +0500

    Refactor generator.ts

--- packages/react-boilerplate-kit/src/generators/preset/generator.ts
+++ packages/react-boilerplate-kit/src/generators/preset/generator.ts
@@ -3,6 +3,7 @@ import {
   addProjectConfiguration,
   formatFiles,
   generateFiles,
+  installPackagesTask,
   Tree,
   updateJson,
 } from '@nx/devkit';
@@ -21,6 +22,16 @@ export async function presetGenerator(
   tree: Tree,
   options: PresetGeneratorSchema
 ) {
+  const dependencies = {
+    react: await getLatestVersion('react'),
+    'react-dom': await getLatestVersion('react-dom'),
+    'react-scripts': await getLatestVersion('react-scripts'),
+  };
+  const devDependencies = {
+    typescript: await getLatestVersion('typescript'),
+    '@types/react': await getLatestVersion('@types/react'),
+    '@types/react-dom': await getLatestVersion('@types/react-dom'),
+  };
   const projectRoot = `.`;
   const targetRoot = projectRoot + '/src';
   addProjectConfiguration(tree, options.name, {
@@ -33,12 +44,8 @@ export async function presetGenerator(
     tmpl: '.template',
   });
   if (options.useReactRouter) {
-    addDependenciesToPackageJson(
-      tree,
-      {
-        'react-router-dom': await getLatestVersion('react-router-dom'),
-      },
-      {}
+    dependencies['react-router-dom'] = await getLatestVersion(
+      'react-router-dom'
     );
     generateFiles(
       tree,
@@ -48,13 +55,9 @@ export async function presetGenerator(
     );
   }
   if (options.useRedux) {
-    addDependenciesToPackageJson(
-      tree,
-      {
-        'react-redux': await getLatestVersion('react-redux'),
-        '@reduxjs/toolkit': await getLatestVersion('@reduxjs/toolkit'),
-      },
-      {}
+    dependencies['react-redux'] = await getLatestVersion('react-redux');
+    dependencies['@reduxjs/toolkit'] = await getLatestVersion(
+      '@reduxjs/toolkit'
     );
     generateFiles(
       tree,
@@ -63,14 +66,65 @@ export async function presetGenerator(
       options
     );
   }
-  if (options.uiLibrary === 'antd') {
-    addDependenciesToPackageJson(
-      tree,
-      {
-        antd: await getLatestVersion('antd'),
-      },
-      {}
+  if (options.useStorybook) {
+    dependencies['@storybook/addon-essentials'] = await getLatestVersion(
+      '@storybook/addon-essentials'
     );
+    dependencies['@storybook/addon-interactions'] = await getLatestVersion(
+      '@storybook/addon-interactions'
+    );
+    dependencies['@storybook/addon-links'] = await getLatestVersion(
+      '@storybook/addon-links'
+    );
+    dependencies['@storybook/addon-onboarding'] = await getLatestVersion(
+      '@storybook/addon-onboarding'
+    );
+    dependencies['@storybook/blocks'] = await getLatestVersion(
+      '@storybook/blocks'
+    );
+    dependencies['@storybook/preset-create-react-app'] = await getLatestVersion(
+      '@storybook/preset-create-react-app'
+    );
+    dependencies['@storybook/react'] = await getLatestVersion(
+      '@storybook/react'
+    );
+    dependencies['@storybook/react-webpack5'] = await getLatestVersion(
+      '@storybook/react-webpack5'
+    );
+    dependencies['@storybook/testing-library'] = await getLatestVersion(
+      '@storybook/testing-library'
+    );
+    dependencies['babel-plugin-named-exports-order'] = await getLatestVersion(
+      'babel-plugin-named-exports-order'
+    );
+    dependencies['eslint-plugin-storybook'] = await getLatestVersion(
+      'eslint-plugin-storybook'
+    );
+    dependencies['prop-types'] = await getLatestVersion('prop-types');
+    dependencies['storybook'] = await getLatestVersion('storybook');
+    dependencies['webpack'] = await getLatestVersion('webpack');
+
+    generateFiles(
+      tree,
+      path.join(__dirname, 'meta_data', 'storybook_components', '_storybook'),
+      projectRoot,
+      options
+    );
+    generateFiles(
+      tree,
+      path.join(__dirname, 'meta_data', 'storybook_components', '_stories'),
+      targetRoot,
+      options
+    );
+    updateJson(tree, 'package.json', (json) => {
+      json.scripts = json.scripts || {};
+      json.scripts.storybook = 'storybook dev -p 6006';
+      json.scripts['build-storybook'] = 'storybook build';
+      return json;
+    });
+  }
+  if (options.uiLibrary === 'antd') {
+    dependencies['antd'] = await getLatestVersion('antd');
     generateFiles(
       tree,
       path.join(__dirname, 'meta_data', 'ui_library_components', 'antd'),
@@ -78,15 +132,9 @@ export async function presetGenerator(
       options
     );
   } else if (options.uiLibrary === 'mui') {
-    addDependenciesToPackageJson(
-      tree,
-      {
-        '@mui/material': await getLatestVersion('@mui/material'),
-        '@emotion/react': await getLatestVersion('@emotion/react'),
-        '@emotion/styled': await getLatestVersion('@emotion/styled'),
-      },
-      {}
-    );
+    dependencies['@mui/material'] = await getLatestVersion('@mui/material');
+    dependencies['@emotion/react'] = await getLatestVersion('@emotion/react');
+    dependencies['@emotion/styled'] = await getLatestVersion('@emotion/styled');
     generateFiles(
       tree,
       path.join(__dirname, 'meta_data', 'ui_library_components', 'mui'),
@@ -110,19 +158,10 @@ export async function presetGenerator(
     return json;
   });
   await formatFiles(tree);
-  addDependenciesToPackageJson(
-    tree,
-    {
-      react: await getLatestVersion('react'),
-      'react-dom': await getLatestVersion('react-dom'),
-      'react-scripts': await getLatestVersion('react-scripts'),
-    },
-    {
-      typescript: await getLatestVersion('typescript'),
-      '@types/react': await getLatestVersion('@types/react'),
-      '@types/react-dom': await getLatestVersion('@types/react-dom'),
-    }
-  );
+  addDependenciesToPackageJson(tree, dependencies, devDependencies);
+  return () => {
+    installPackagesTask(tree);
+  };
 }
 
 export default presetGenerator;
